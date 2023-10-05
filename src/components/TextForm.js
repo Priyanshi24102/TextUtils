f929abf61555fa6956aec9a15090789f6672301a
Priyanshi Chouhan <priyanshichouhan24@gmail.com>
2023-10-05 14:17:20 +0530

    Fixed word count issue

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -58,7 +58,7 @@ export default function TextForm(props) {
 
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>Your text summary</h1>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{text.split( /\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>Number of sentences: {text.split(".").length -1 }</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read</p>
         <h2>Preview</h2>
